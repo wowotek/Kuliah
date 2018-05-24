3b03804f770a94ef389a2faada69721368786d50
Wowotek <erlanggaibr2@gmail.com>
2018-05-25 04:49:57 +0700

    Added More Processing

---
+++ Sistem Informasi/Belajar/Programming/Processing/Flappy/build/source/Flappy.java
@@ -0,0 +1,33 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Flappy extends PApplet {
+
+public void setup(){
+  
+}
+
+public void draw(){
+  background(random(255), random(255), random(255));
+}
+  public void settings() {  size(800, 600); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Flappy" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
