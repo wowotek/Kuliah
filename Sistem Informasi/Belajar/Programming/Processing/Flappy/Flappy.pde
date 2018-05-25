b076640257637b590062f747f60b32e92e110367
Wowotek <erlanggaibr2@gmail.com>
2018-05-26 02:15:36 +0700

    Flappy1

--- Sistem Informasi/Belajar/Programming/Processing/Flappy/Flappy.pde
+++ Sistem Informasi/Belajar/Programming/Processing/Flappy/Flappy.pde
@@ -1,7 +1,30 @@
+private Bird bird;
+
 void setup(){
   size(800, 600);
+  this.bird = new Bird(width/2, height/2);
+  frameRate(60);
 }
 
 void draw(){
-  background(0);
+  background(255);
+  this.bird.render();
+}
+
+void keyReleased(){
+  if (this.bird.headingSpeed >= 0.001)
+  {
+    this.bird.headingSpeed -= 0.002;
+  }
+  else if (this.bird.headingSpeed <=0 0.001)
+  {
+    this.bird.headingSpeed += 0.002;
+  }
+}
+void keyPressed(){
+  if(key == 'd'){
+    this.bird.headingSpeed += 0.001;
+  } else if(key == 'a') {
+    this.bird.headingSpeed -= 0.001;
+  }
 }
--- Sistem Informasi/Belajar/Programming/Processing/Flappy/build/source/Flappy.java
+++ Sistem Informasi/Belajar/Programming/Processing/Flappy/build/source/Flappy.java
@@ -14,12 +14,61 @@ import java.io.IOException;
 
 public class Flappy extends PApplet {
 
+private Bird bird;
+
 public void setup(){
   
+  this.bird = new Bird(width/2, height/2);
+  frameRate(60);
 }
 
 public void draw(){
-  background();
+  background(255);
+  this.bird.render();
+}
+
+public void keyReleased(){
+  if (this.bird.headingSpeed >= 0.001f)
+  {
+    this.bird.headingSpeed -= 0.002f;
+  }
+  else if (this.bird.headingSpeed <= 0.001f)
+  {
+    this.bird.headingSpeed += 0.002f;
+  }
+}
+public void keyPressed(){
+  if(key == 'd'){
+    this.bird.headingSpeed += 0.001f;
+  } else if(key == 'a') {
+    this.bird.headingSpeed -= 0.001f;
+  }
+}
+public class Bird{
+  public float x = 0;
+  public float y = 0;
+  public float headingSpeed = 0;
+
+  private float heading = 0;
+
+  public Bird(float initX, float initY){
+    this.x = initX;
+    this.y = initY;
+  }
+
+  public void render(){
+    translate(this.x, this.y);
+    rotate(heading);
+    fill(0);
+    triangle(-10, 10, 10, 10, 0, -10);
+
+    turn();
+  }
+
+  private void turn(){
+    this.heading += this.headingSpeed;
+    println(this.heading + " | " + this.headingSpeed);
+  }
 }
   public void settings() {  size(800, 600); }
   static public void main(String[] passedArgs) {
